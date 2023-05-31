f14c3ff8035d9083bd95ef90a13ed1e2ab1948bb
mariia.kraievska <mariia.kraievska@onix-systems.com>
2023-06-01 00:40:40 +0300

    refactoring

--- main.js
+++ main.js
@@ -1,37 +1,32 @@
 "use strict";
 
-let gl; // The webgl context.
-let surface; // A surface model
-let shProgram; // A shader program
-let spaceball; // A SimpleRotator object that lets the user rotate the view by mouse.
+let gl;
+let surface;
+let shProgram;
+let spaceball;
 let texture;
 let stereoCamera;
 
 let webCamTexture, webCamVideo, webCamBackground;
 
-let handlePosition = 0.0;
-
 const texturePoint = { x: 100, y: 400 };
 
 const scale = 8;
-const teta = Math.PI/2;
+const teta = Math.PI / 2;
 const a0 = 0;
 const r = 1;
 const c = 2;
 const d = 1;
 
-const deg2rad = (angle) => {
-  return (angle * Math.PI) / 180;
-};
+class Model {
+  constructor(name) {
+    this.name = name;
+    this.iVertexBuffer = gl.createBuffer();
+    this.iTextureBuffer = gl.createBuffer();
+    this.count = 0;
+  }
 
-// Constructor
-function Model(name) {
-  this.name = name;
-  this.iVertexBuffer = gl.createBuffer();
-  this.iTextureBuffer = gl.createBuffer();
-  this.count = 0;
-
-  this.BufferData = function (vertices, textures) {
+  bufferData(vertices, textures) {
     gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
 
@@ -42,9 +37,9 @@ function Model(name) {
     gl.vertexAttribPointer(shProgram.iTextureCoords, 2, gl.FLOAT, false, 0, 0);
 
     this.count = vertices.length / 3;
-  };
+  }
 
-  this.Draw = function () {
+  draw() {
     gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
     gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(shProgram.iAttribVertex);
@@ -53,23 +48,22 @@ function Model(name) {
     gl.vertexAttribPointer(shProgram.iTextureCoords, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(shProgram.iTextureCoords);
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, this.count);
-  };
+  }
 }
 
-// Constructor
-function ShaderProgram(name, program) {
-  this.name = name;
-  this.prog = program;
-  // Location of the attribute variable in the shader program.
-  this.iAttribVertex = -1;
-  // Location of the uniform matrix representing the combined transformation.
-  this.iModelViewProjectionMatrix = -1;
-  this.iTextureCoords = -1;
-  this.iTextureU = -1;
+class ShaderProgram {
+  constructor(name, program) {
+    this.name = name;
+    this.prog = program;
+    this.iAttribVertex = -1;
+    this.iModelViewProjectionMatrix = -1;
+    this.iTextureCoords = -1;
+    this.iTextureU = -1;
+  }
 
-  this.Use = function () {
+  use() {
     gl.useProgram(this.prog);
-  };
+  }
 }
 
 const leftFrustum = (stereoCamera) => {
@@ -100,6 +94,7 @@ const rightFrustum = (stereoCamera) => {
 
   const left = (-c * near) / convergence;
   const right = (b * near) / convergence;
+
   return m4.orthographic(left, right, bottom, top, near, far);
 };
 
@@ -142,7 +137,7 @@ function draw() {
     gl.UNSIGNED_BYTE,
     webCamVideo
   );
-  webCamBackground?.Draw();
+  webCamBackground?.draw();
 
   gl.bindTexture(gl.TEXTURE_2D, texture);
   gl.clear(gl.DEPTH_BUFFER_BIT);
@@ -153,90 +148,92 @@ function draw() {
   gl.uniformMatrix4fv(shProgram.iModelViewMatrix, false, matAccumLeft);
   gl.uniformMatrix4fv(shProgram.iProjectionMatrix, false, projectionLeft);
   gl.colorMask(true, false, false, false);
-  surface.Draw();
+  surface.draw();
 
   gl.clear(gl.DEPTH_BUFFER_BIT);
 
   gl.uniformMatrix4fv(shProgram.iModelViewMatrix, false, matAccumRight);
   gl.uniformMatrix4fv(shProgram.iProjectionMatrix, false, projectionRight);
   gl.colorMask(false, true, true, false);
-  surface.Draw();
+  surface.draw();
 
   gl.colorMask(true, true, true, true);
 }
 
-const step = (max, splines = 20) => {
-  return max / (splines - 1);
-};
-
-const cos = (x) => {
-  return Math.cos(x);
-};
-
-const sin = (x) => {
-  return Math.sin(x);
-};
-
-function getX (t,a, param = 15) {
-    return ((r * Math.cos(a) - (r * (a0 - a) + t * Math.cos(teta) - c * Math.sin(d * t) * Math.sin(teta)) * Math.sin(a)) / param) * scale;
-}
-function getY (t,a, param = 15) {
-    return ((r * Math.sin(a) + (r * (a0 - a) + t * Math.cos(teta) - c * Math.sin(d * t) * Math.sin(teta)) * Math.cos(a)) / param) * scale;
-}
-function getZ (t, height = 15) {
-    return ((t * Math.sin(teta) + c * Math.sin(d * t) * Math.cos(teta)) / (-height)) * scale;
-}
-
 function CreateSurfaceData() {
-    let vertexList = [];
-    let normalsList = [];
+  let vertexList = [];
+  let normalsList = [];
 
-    let deltaT = 0.0005;
-    let deltaA = 0.0005;
+  let deltaT = 0.0005;
+  let deltaA = 0.0005;
 
-    const step = 0.5
+  const step = 0.1;
 
-    for (let t = -15; t <= 15; t += step) {
-        for (let a = 0; a <= 15; a += step) {
-            const tNext = t + step;
-            vertexList.push(getX(t, a, 10), getY(t, a, 10), getZ(t, 20));
-            vertexList.push(getX(tNext, a, 10), getY(tNext, a, 10), getZ(tNext, 20));
+  for (let t = -15; t <= 15; t += step) {
+    for (let a = 0; a <= 15; a += step) {
+      const tNext = t + step;
+      vertexList.push(getX(t, a, 10), getY(t, a, 10), getZ(t, 20));
+      vertexList.push(getX(tNext, a, 10), getY(tNext, a, 10), getZ(tNext, 20));
 
-            let result = m4.cross(calcDerT(t, a, deltaT), calcDerA(t, a, deltaA));
-            normalsList.push(result[0], result[1], result[2])
+      let result = m4.cross(calcDerT(t, a, deltaT), calcDerA(t, a, deltaA));
+      normalsList.push(result[0], result[1], result[2]);
 
-            result = m4.cross(calcDerT(tNext, a, deltaT), calcDerA(tNext, a, deltaA));
-            normalsList.push(result[0], result[1], result[2]);
-        }
+      result = m4.cross(calcDerT(tNext, a, deltaT), calcDerA(tNext, a, deltaA));
+      normalsList.push(result[0], result[1], result[2]);
     }
+  }
 
+  return [vertexList, normalsList];
+}
 
-    return [vertexList, normalsList];
+function getX(t, a, param = 15) {
+  return (
+    ((r * Math.cos(a) -
+      (r * (a0 - a) + t * Math.cos(teta) - c * Math.sin(d * t) * Math.sin(teta)) *
+        Math.sin(a)) /
+      param) *
+    scale
+  );
+}
+
+function getY(t, a, param = 15) {
+  return (
+    ((r * Math.sin(a) +
+      (r * (a0 - a) + t * Math.cos(teta) - c * Math.sin(d * t) * Math.sin(teta)) *
+        Math.cos(a)) /
+      param) *
+    scale
+  );
+}
+
+function getZ(t, height = 15) {
+  return (
+    ((t * Math.sin(teta) + c * Math.sin(d * t) * Math.cos(teta)) / -height) *
+    scale
+  );
 }
 
 const calcDerT = (t, a, tDelta) => ([
-    (getX(t + tDelta, a, 10) - getX(t, a, 10)) / degriesToRadians(tDelta),
-    (getY(t + tDelta, a, 10) - getY(t, a, 10)) / degriesToRadians(tDelta),
-    (getZ(t + tDelta, a) - getZ(t, a)) / degriesToRadians(tDelta),
-])
+  (getX(t + tDelta, a, 10) - getX(t, a, 10)) / degriesToRadians(tDelta),
+  (getY(t + tDelta, a, 10) - getY(t, a, 10)) / degriesToRadians(tDelta),
+  (getZ(t + tDelta, a) - getZ(t, a)) / degriesToRadians(tDelta),
+]);
 
 const calcDerA = (t, a, aDelta) => ([
-    (getX(t, a + aDelta, 10) - getX(t, a, 10)) / degriesToRadians(aDelta),
-    (getY(t, a + aDelta, 10) - getY(t, a, 10)) / degriesToRadians(aDelta),
-    (getZ(t, a + aDelta) - getZ(t, a)) / degriesToRadians(aDelta),
-])
-
+  (getX(t, a + aDelta, 10) - getX(t, a, 10)) / degriesToRadians(aDelta),
+  (getY(t, a + aDelta, 10) - getY(t, a, 10)) / degriesToRadians(aDelta),
+  (getZ(t, a + aDelta) - getZ(t, a)) / degriesToRadians(aDelta),
+]);
 
 function degriesToRadians(angle) {
-    return angle * Math.PI / 180;
+  return (angle * Math.PI) / 180;
 }
 
-/* Initialize the WebGL context. Called from init() */
 function initGL() {
   let prog = createProgram(gl, vertexShaderSource, fragmentShaderSource);
 
   shProgram = new ShaderProgram("Basic", prog);
-  shProgram.Use();
+  shProgram.use();
 
   shProgram.iAttribVertex = gl.getAttribLocation(prog, "vertex");
   shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(
@@ -251,8 +248,8 @@ function initGL() {
 
   surface = new Model("Surface");
   let data = CreateSurfaceData();
-  console.log(data[0].length)
-  surface.BufferData(data[0], data[1]);
+  surface.bufferData(data[0], data[1]);
+
   const image = new Image();
   image.crossOrigin = "anonymous";
   image.src =
@@ -267,7 +264,7 @@ function initGL() {
   });
 
   webCamBackground = new Model("Background");
-  webCamBackground.BufferData(
+  webCamBackground.bufferData(
     [
       0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0,
       0.0, 0.0, 0.0,
@@ -278,40 +275,32 @@ function initGL() {
   gl.enable(gl.DEPTH_TEST);
 }
 
-/* Creates a program for use in the WebGL context gl, and returns the
- * identifier for that program.  If an error occurs while compiling or
- * linking the program, an exception of type Error is thrown.  The error
- * string contains the compilation or linking error.  If no error occurs,
- * the program identifier is the return value of the function.
- * The second and third parameters are strings that contain the
- * source code for the vertex shader and for the fragment shader.
- */
 function createProgram(gl, vShader, fShader) {
   let vsh = gl.createShader(gl.VERTEX_SHADER);
   gl.shaderSource(vsh, vShader);
   gl.compileShader(vsh);
   if (!gl.getShaderParameter(vsh, gl.COMPILE_STATUS)) {
-    throw new Error("Error in vertex shader:  " + gl.getShaderInfoLog(vsh));
+    throw new Error("Error in vertex shader: " + gl.getShaderInfoLog(vsh));
   }
+
   let fsh = gl.createShader(gl.FRAGMENT_SHADER);
   gl.shaderSource(fsh, fShader);
   gl.compileShader(fsh);
   if (!gl.getShaderParameter(fsh, gl.COMPILE_STATUS)) {
-    throw new Error("Error in fragment shader:  " + gl.getShaderInfoLog(fsh));
+    throw new Error("Error in fragment shader: " + gl.getShaderInfoLog(fsh));
   }
+
   let prog = gl.createProgram();
   gl.attachShader(prog, vsh);
   gl.attachShader(prog, fsh);
   gl.linkProgram(prog);
   if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
-    throw new Error("Link error in program:  " + gl.getProgramInfoLog(prog));
+    throw new Error("Link error in program: " + gl.getProgramInfoLog(prog));
   }
+
   return prog;
 }
 
-/**
- * initialization function that will be called when the page has loaded
- */
 function init() {
   let canvas;
   try {
@@ -333,14 +322,13 @@ function init() {
       "<p>Sorry, could not get a WebGL graphics context.</p>";
     return;
   }
+
   try {
-    initGL(); // initialize the WebGL graphics context
+    initGL();
   } catch (e) {
     console.log(e);
     document.getElementById("canvas-holder").innerHTML =
-      "<p>Sorry, could not initialize the WebGL graphics context: " +
-      e +
-      "</p>";
+      "<p>Sorry, could not initialize the WebGL graphics context: " + e + "</p>";
     return;
   }
 
@@ -381,3 +369,5 @@ const createWebCamTexture = () => {
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
   return textureID;
 };
+
+init();
